94b2f73e00d95a8d959b7a81ea4d68fc8d018e53
Vidar Alvestad <vidar.alvestad@skatteetaten.no>
2024-01-29 14:55:26 +0100

    Updates

--- component---src-pages-404-js-e4eccf023c99b88d51c9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a);t.default=()=>l.a.createElement("div",null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist."))}}]);
-//# sourceMappingURL=component---src-pages-404-js-e4eccf023c99b88d51c9.js.map
